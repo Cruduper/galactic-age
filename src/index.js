472ce4b9619349adee0873fb517a9dbb42e7a44b
Eric Crudup <briefcasemanx@gmail.com>
2022-02-04 14:52:18 -0800

    WIP debugging not working in browser

--- src/index.js
+++ src/index.js
@@ -4,10 +4,36 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Human from './js/human.js';
 
+function correctInput(userValArr){
+  inputsCorrect = true;
+  userValArr.forEach((inputVal) =>{
+    if( (inputVal >100) || (inputVal < 0) ){
+      inputsCorrect = false;
+    }
+    return inputsCorrect;
+  });
 
-
-
+}
 
 $("#submit").click( function(){
-  createHuman();
+  alert("we're in the .click function!");
+  const userInputs = [5];
+  nameVar = $("#name").val();
+  userInputs[0] = $("#age").val();
+  smokeVar = $("#smoke").val();
+  userInputs[1] = $("#activity").val();
+  userInputs[2] = $("#combat").val();
+  userInputs[3] = $("#intelligence").val();
+  userInputs[4] = $("#rad").val();
+
+  if ( correctInput( userInputs) ){
+    user = new Human(nameVar, userInputs[0], smokeVar, userInputs[1], userInputs[2], userInputs[3], userInputs[4]);
+    //$("userInput").hide();
+  } else {
+    alert("You entered an incorrect value, try again!");
+  }
+ 
+
+
+
 });
